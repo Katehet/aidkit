a33a6764e437b3cd7347ca18f460cc08674e9f80
svsleep <sv_sleep@mail.ru>
2022-05-07 22:18:56 +0300

    Create code for new rows to db writing

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'db.php';
+
+$name = $_POST['name'] ?? "";
+$number = $_POST['number'] ?? NULL;
+$type = $_POST['type'] ?? "";
+$exp_date = $_POST['exp_date'] ?? NULL;
+$get_id = $_GET['id'] ?? "";
+
+
+
+//Create
+
+if (isset($_POST['add'])) {
+
+    $sql = ('INSERT INTO medicines (med_name, med_number, med_type, exp_date) VALUES (?, ?, ?, ?)');
+    $query = $pdo->prepare($sql);
+    $query->execute([$name, $number, $type, $exp_date]);
+    
+    if ($query) {
+        header('Location: '.$_SERVER['HTTP_REFERER']);
+    }
+
+}
