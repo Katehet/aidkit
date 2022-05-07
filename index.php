4363676c6d5ac3004bc1294e04d11cdff682bdf6
svsleep <sv_sleep@mail.ru>
2022-05-07 22:42:23 +0300

    Corrects modal #create and adds loop for output rows

--- index.php
+++ index.php
@@ -33,17 +33,80 @@
                 </tr>
             </thead>
             <tbody>
+                <?php foreach ($result as $res) { ?>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Амбробене (30мг)</td>
-                    <td>14</td>
-                    <td>таб.</td>
-                    <td>01.04.2023</td>
+                    <th scope="row"><?php echo $res->med_id; ?></th>
+                    <td><?php echo $res->med_name; ?></td>
+                    <td><?php echo $res->med_number; ?></td>
+                    <td><?php echo $res->med_type; ?></td>
+                    <td><?php echo $res->exp_date; ?></td>
                     <td>
-                        <a href="" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#edit"><i class="fa fa-edit"></i></a>
+                        <a href="?id=<?php echo $res->med_id; ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#edit<?php echo $res->med_id; ?>"><i class="fa fa-edit"></i></a>
                         <a href="" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete"><i class="fa fa-trash-alt"></i></a>
                     </td>
                 </tr>
+                <!-- Modal edit -->
+                <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Что нужно изменить?</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Название:</label>
+                                        <input type="text" class="form-control" name="name"> id="name">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Количество:</label>
+                                        <input type="text" class="form-control" name="number" id="number">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Вид:</label>
+                                        <input type="text" class="form-control" name="type" id="type">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Срок годности:</label>
+                                        <input type="date" class="form-control" name="exp_date" id="exp_date">
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отменить</button>
+                                        <button type="submit" class="btn btn-success" name="edit">Сохранить</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Modal edit -->
+                <!-- Modal delete -->
+                <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Удаляем? Вы уверены?</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row row-cols-1 mx-lg-n1">
+                                    <div class="col py-3 px-2">Название:</div>
+                                    <div class="col py-3 px-2">Количество:</div>
+                                    <div class="col py-3 px-2">Срок до:</div>
+                                </div>
+                                <form>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
+                                        <button type="submit" class="btn btn-danger">Удалить</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Modal delete -->
+                <?php }; ?>
             </tbody>
         </table>
     </div>
@@ -85,67 +148,6 @@
         </div>
     </div>
     <!-- Modal create -->
-    <!-- Modal edit -->
-    <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Что нужно изменить?</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Название:</label>
-                            <input type="text" class="form-control" id="name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Количество:</label>
-                            <input type="text" class="form-control" id="number">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Вид:</label>
-                            <input type="text" class="form-control" id="type">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Срок годности:</label>
-                            <input type="date" class="form-control" id="expiry">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отменить</button>
-                            <button type="submit" class="btn btn-success">Сохранить</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal edit -->
-    <!-- Modal delete -->
-    <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Удаляем? Вы уверены?</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="row row-cols-1 mx-lg-n1">
-                        <div class="col py-3 px-2">Название:</div>
-                        <div class="col py-3 px-2">Количество:</div>
-                        <div class="col py-3 px-2">Срок до:</div>
-                    </div>
-                    <form>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
-                            <button type="submit" class="btn btn-danger">Удалить</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal delete -->
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
