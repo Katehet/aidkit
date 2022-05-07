dece1e4c2db12db889505d4411369804cb7504fc
svsleep <sv_sleep@mail.ru>
2022-05-07 23:00:19 +0300

    Adds code for edit&output db data

--- functions.php
+++ functions.php
@@ -29,3 +29,16 @@ if (isset($_POST['add'])) {
 $sql = $pdo->prepare("SELECT * FROM medicines");
 $sql->execute();
 $result = $sql->fetchAll(PDO::FETCH_OBJ);
+
+//Update
+
+if (isset($_POST['edit'])) {
+
+    $sql = ("UPDATE medicines SET med_name=?, med_number=?, med_type=?, exp_date=? WHERE med_id=?");
+    $query = $pdo->prepare($sql);
+    $query->execute([$name, $number, $type, $exp_date, $get_id]);
+
+    if ($query) {
+        header("Location: ".$_SERVER['HTTP_REFERER']);
+    }
+}
